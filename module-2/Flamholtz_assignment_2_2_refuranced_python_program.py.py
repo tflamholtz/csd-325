9979b26ac3bcc61386caf91c1c594de74a92ce9f
tflamholtz <tflamholtz@my365.bellevue.edu>
2026-06-21 19:20:59 -0400

    Pushing assignment 2_2 06/21/2026

---
+++ module-2/Flamholtz_assignment_2_2_refuranced_python_program.py.py
@@ -0,0 +1,40 @@
+#Program that converts gallons to liters
+
+
+def main():
+    #Display intro screen
+    intro()
+
+    #try:
+    #Get the number of gallons
+    gallons = float(input("Enter the number of gallons"))
+
+    #Convert the gallons to liters
+    gallons_to_liters(gallon)
+
+
+    #except:
+        #print("An exception occured, try again by entering only a number")
+        #print()
+        #main()
+
+
+    #The intro function displays an introductory screen
+def intro():
+    print("This program converts measurments")
+    print("in gallons to liters")
+    print("For referance the formula is")
+    print("1 gallon =  3.785412 liters")
+    print()
+
+
+    #The gallons_to_liters function accepts a number of gallons
+    #and displays the equivalent number of liters
+
+def gallons_to_liters(gallons):
+    liters = gallons *  3.785412
+    print(f"That converts to {liters} liters")
+
+
+    #Call the main function
+main()
