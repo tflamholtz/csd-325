51095e0ce766a296fabc9daad8df69c9cbb344eb
tflamholtz <tflamholtz@my365.bellevue.edu>
2026-07-19 15:16:51 -0400

    Pushing 07/19/2026

---
+++ module-6/forestfiresim_325.py
@@ -0,0 +1,150 @@
+"""Forest Fire Sim, modified by Sue Sampson, based on a program by Al Sweigart
+A simulation of wildfires spreading in a forest. Press Ctrl-C to stop.
+Inspired by Nicky Case's Emoji Sim http://ncase.me/simulating/model/
+** use spaces, not indentation to modify **
+Tags: short, bext, simulation"""
+
+import random, sys, time
+
+try:
+    import bext
+except ImportError:
+    print('This program requires the bext module, which you')
+    print('can install by following the instructions at')
+    print('https://pypi.org/project/Bext/')
+    sys.exit()
+
+# Set up the constants:
+WIDTH = 79
+HEIGHT = 22
+
+TREE = 'A'
+FIRE = '@'
+EMPTY = ' '
+#7/13/2026 - Added WATER constant -Austin.L
+WATER = 'W'
+
+#7/13/2026 - Water Dimensions for initial forest object -Austin.L
+#Using dimensions shown in example
+WATER_WIDTH = 5
+WATER_HEIGHT = 10
+WATER_BOTTOM_MARGIN = 2
+
+
+# (!) Try changing these settings to anything between 0.0 and 1.0:
+INITIAL_TREE_DENSITY = 0.20  # Amount of forest that starts with trees.
+GROW_CHANCE = 0.01  # Chance a blank space turns into a tree.
+FIRE_CHANCE = 0.01  # Chance a tree is hit by lightning & burns.
+
+# (!) Try setting the pause length to 1.0 or 0.0:
+PAUSE_LENGTH = 0.5
+
+
+def main():
+    forest = createNewForest()
+    bext.clear()
+
+    while True:  # Main program loop.
+        displayForest(forest)
+
+        # Run a single simulation step:
+        nextForest = {'width': forest['width'],
+                      'height': forest['height']}
+
+        for x in range(forest['width']):
+            for y in range(forest['height']):
+                if (x, y) in nextForest:
+                    # If we've already set nextForest[(x, y)] on a
+                    # previous iteration, just do nothing here:
+                    continue
+
+                if ((forest[(x, y)] == EMPTY)
+                    and (random.random() <= GROW_CHANCE)):
+                    # Grow a tree in this empty space.
+                    nextForest[(x, y)] = TREE
+                elif ((forest[(x, y)] == TREE)
+                    and (random.random() <= FIRE_CHANCE)):
+                    # Lightning sets this tree on fire.
+                    nextForest[(x, y)] = FIRE
+                elif forest[(x, y)] == FIRE:
+                    # This tree is currently burning.
+                    # Loop through all the neighboring spaces:
+                    for ix in range(-1, 2):
+                        for iy in range(-1, 2):
+                            # Fire spreads to neighboring trees:
+                            if forest.get((x + ix, y + iy)) == TREE:
+                                nextForest[(x + ix, y + iy)] = FIRE
+                    # The tree has burned down now, so erase it:
+                    nextForest[(x, y)] = EMPTY
+                elif forest[(x, y)] == WATER:
+                    #7/16/2026 - Prevent any potential overwrite of WATER
+                    nextForest[(x, y)] = WATER
+                else:
+                    # Just copy the existing object:
+                    nextForest[(x, y)] = forest[(x, y)]
+        forest = nextForest
+
+        time.sleep(PAUSE_LENGTH)
+
+
+def createNewForest():
+    """Returns a dictionary for a new forest data structure."""
+    forest = {'width': WIDTH, 'height': HEIGHT}
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            if (random.random() * 100) <= INITIAL_TREE_DENSITY:
+                forest[(x, y)] = TREE  # Start as a tree.
+            else:
+                forest[(x, y)] = EMPTY  # Start as an empty space.
+
+    #7/13/2026 - Overwrites anything created in designated water space.
+    for coords in getWaterPlacementCoords():
+        forest[coords] = WATER
+    return forest
+
+
+def displayForest(forest):
+    """Display the forest data structure on the screen."""
+    bext.goto(0, 0)
+    for y in range(forest['height']):
+        for x in range(forest['width']):
+            if forest[(x, y)] == TREE:
+                bext.fg('green')
+                print(TREE, end='')
+            elif forest[(x, y)] == FIRE:
+                bext.fg('red')
+                print(FIRE, end='')
+            elif forest[(x, y)] == EMPTY:
+                print(EMPTY, end='')
+            #7/13/2026 - Added WATER coloring
+            elif forest[(x, y)] == WATER:
+                bext.fg('blue')
+                print(WATER, end='')
+        print()
+    bext.fg('reset')  # Use the default font color.
+    print('Grow chance: {}%  '.format(GROW_CHANCE * 100), end='')
+    print('Lightning chance: {}%  '.format(FIRE_CHANCE * 100), end='')
+    print('Press Ctrl-C to quit.')
+
+def getWaterPlacementCoords():
+    """Returns a dictionary for a water placement data structure,
+    centered horizontally and two spaces from the bottom of the screen"""
+    start_x = (WIDTH - WATER_WIDTH) // 2
+    end_x = start_x + WATER_WIDTH
+    end_y = HEIGHT - WATER_BOTTOM_MARGIN
+    start_y = end_y - WATER_HEIGHT
+
+    #7/13/2026 - Uses the width and height of the forest object to create the water block.
+    coords = set()
+    for x in range(start_x, end_x):
+        for y in range(start_y, end_y):
+            coords.add((x, y))
+    return coords
+
+
+# If this program was run (instead of imported), run the game:
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit()  # When Ctrl-C is pressed, end the program.
